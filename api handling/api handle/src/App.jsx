91363a47d3d912120d2a664ddf6aad3bc54d57a7
Sagar saini <sagarsaini954818@gmail.com>
2025-10-17 16:21:40 +0530

    axios api handle

--- api handling/api handle/src/App.jsx
+++ api handling/api handle/src/App.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState([]);
 
-const getData = async () =>{
-  
-const respons = await fetch('https://picsum.photos/v2/list')
-const data = await respons.json()
- data.map((elem)=>{
-     return console.log(elem.url)
- })
+  const getData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
 
-}
+    const data = response.data;
+
+    setData(data);
+    
+  };
   return (
-    <div>
-      <button onClick={()=>{
-        getData()
-      }} >Get Data</button>
-    </div>
-  )
-}
+    <>
+      <div>
+        <button
+          onClick={() => {
+            getData();
+          }}
+        >
+          Get Data
+        </button>
+      </div>
 
-export default App
+      {data.map((e,indx) => {
+        return <h3>hello ,{indx},{e.url}</h3>;
+      })}
+    </>
+  );
+};
+
+export default App;
